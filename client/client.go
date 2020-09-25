8dcf99ed234372aa81a1b77f2436e61c1ee455f6
Artem V. Zaborskiy <ftomza@yandex.ru>
2020-09-25 20:50:23 +0300

    Add Copyright

--- client/client.go
+++ client/client.go
@@ -1,7 +1,15 @@
+/*
+ * Copyright © 2020-present Artem V. Zaborskiy <ftomza@yandex.ru>. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found
+ * in the LICENSE file in the root directory of this source tree.
+ */
+
 package client
 
 import (
 	"errors"
+
 	"github.com/ftomza/go-sspvo"
 	"github.com/ftomza/go-sspvo/message"
 )
--- sspvo.go
+++ sspvo.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2020-present Artem V. Zaborskiy <ftomza@yandex.ru>. All rights reserved.
+ *
+ * This source code is licensed under the Apache 2.0 license found
+ * in the LICENSE file in the root directory of this source tree.
+ */
+
 package sspvo
 
 import (
