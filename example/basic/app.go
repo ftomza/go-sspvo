5a3aee59d836882ef920e5cf6fa0324a23da00e1
Artem V. Zaborskiy <ftomza@yandex.ru>
2020-10-16 20:32:11 +0300

    UPD: update base example for use test server "EPGU"

--- example/basic/app.go
+++ example/basic/app.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"time"
 
+	"github.com/ftomza/go-sspvo/test_server_epgu"
+
 	"github.com/ftomza/go-sspvo/client"
 	"github.com/ftomza/go-sspvo/message"
 
@@ -12,8 +14,11 @@ import (
 )
 
 func main() {
+
+	go test_server_epgu.RunServerDefault()
+
 	restyClient := resty.New()
-	restyClient.SetHostURL("http://85.142.162.12:8031")
+	restyClient.SetHostURL("http://localhost:7777")
 	restyClient.SetDebug(false)
 	sspvoClient, err := client.NewRestyClient(restyClient,
 		client.SetAPIBase("/api"),
@@ -27,7 +32,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	data, err := sspvoClient.Send(ctx, message.NewCLSMessage("Directions")).Data()
+	data, err := sspvoClient.Send(ctx, message.NewCLSMessage(message.CLSLevelBudget)).Data()
 	if err != nil {
 		log.Fatal(err, string(data))
 	}
